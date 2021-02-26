08db22cc7d3ec6080701d4fc40ca4d5629fbd0f5
Jack Ryan <jackryan1998@hotmail.com>
2021-02-26 14:54:51 +1100

    background fix

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
 import React, {Component} from "react";
-import { Layout, Avatar, Typography, Card, Col, Row, List }  from 'antd';
 import 'antd/dist/antd.css';
+import { Layout, Avatar, Typography, Card, Col, Row, List }  from 'antd';
 import {FaMapMarkerAlt, FaEnvelopeOpen, FaGraduationCap, FaReact} from "react-icons/fa";
 import profilePic from './profile.jpg'
+import Background from './codeback.jpg'
 
 const { Content, Footer } = Layout;
 const { Title } = Typography;
 const technologies = [
     'Python','Java','C/C++','Ruby on Rails', 'JavaScript', 'React', 'Android'
-
 ];
 
 const skills = [
     'Google Cloud Platform', 'AWS', 'Heroku', 'Docker', 'Machine Learning', 'SQL'
+];
 
+const project = [
+    'Basketball Messenger Bot', 'Pacman Contest', 'In Progress: Splyce'
 ];
 
 export default class App extends Component{
     render() {
         return(
             <Layout>
-                <Content style={{height: '100vh', }}>
-                    <br/><br/>
+                <Content style={{height: '100%', backgroundColor: "light-grey" }}>
+                    <div style={{backgroundImage: `url(${Background})`, backgroundSize: 'cover', overflow: 'visible'}}>
+                        <br/><br/>
                     <div style={{display: 'flex', justifyContent: 'center'}}>
                         <Avatar
                             style={{alignItems: 'middle'}}
-                            size={128}
+                            size={200}
                             src={profilePic}
                         />
                     </div>
+
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                    </div>
                     <br/>
-                    <br/>
+
+
                     <div style={{display: 'flex', justifyContent: 'center'}}>
                         <Title >Jack Ryan</Title>
                     </div>
@@ -47,11 +58,12 @@ export default class App extends Component{
                         <p> <FaGraduationCap/> Bachelor of Computer Science @ RMIT University </p>
                     </div>
 
+
                     <br/>
-                    <div className="site-card-wrapper">
-                        <Row gutter={16}>
-                            <Col span={8}>
-                                <Card title="Technologies" bordered={false}>
+                    <div className="site-card-wrapper" >
+                        <Row justify="space-around">
+                            <Col span={7}>
+                                <Card title="Technologies" hoverable={true} bordered={false}>
                                     <List
                                         dataSource={technologies}
                                         renderItem={item => <List.Item>{item}</List.Item>}
@@ -59,8 +71,8 @@ export default class App extends Component{
                                     />
                                 </Card>
                             </Col>
-                            <Col span={8}>
-                                <Card title="Skills" bordered={false}>
+                            <Col span={7}>
+                                <Card title="Skills" hoverable={true} bordered={false}>
                                     <List
                                         dataSource={skills}
                                         renderItem={item => <List.Item>{item}</List.Item>}
@@ -68,18 +80,25 @@ export default class App extends Component{
                                     />
                                 </Card>
                             </Col>
-                            <Col span={8}>
-                                <Card title="Projects" bordered={false}>
-                                    Basketball Messenger Bot
+                            <Col span={7}>
+                                <Card title="Projects" hoverable={true} bordered={false}>
+                                    <List
+                                        dataSource={project}
+                                        renderItem={item => <List.Item>{item}</List.Item>}
+
+                                    />
                                 </Card>
                             </Col>
                         </Row>
                     </div>
+
+                    <br/><br/>
+
                 </Content>
                 <Footer
                     style={{display: 'flex', justifyContent: 'center',
-                    left: 0, bottom: 0, width: '100%' }}>
-                    This website was built using React <FaReact/></Footer>
+                    left: 0, bottom: 0 }}>
+                    <p>This website was built using React <FaReact/></p> <p> +  <a href={"https://ant.design/"}> ant.design</a> </p> </Footer>
             </Layout>
         );
     }
